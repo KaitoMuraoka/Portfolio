23215cd8f9fec929cf0e202c83610b117f10c017
KaitoMuraoka <kaitoboshi@icloud.com>
2024-03-25 15:22:30 +0900

    All Comment out

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -23,38 +23,38 @@ struct Home: StaticPage {
 
         Text("Before you create sites yourself, you should review some key concepts that underpin how Ignite works:")
 
-        List {
-            Link("Grid Layout", target: GridExamples())
-            Link("Navigation", target: NavigationExamples())
-            Link("Content", target: ContentExamples())
-            Link("Text", target: TextExamples())
-            Link("Styling", target: StylingExamples())
-        }
-        .listStyle(.ordered(.default))
-
-
-        Text("Examples")
-            .font(.title2)
-            .margin(.top, .large)
-
-        List {
-            Link("Accordions", target: AccordionExamples())
-            Link("Alerts", target: AlertExamples())
-            Link("Badges", target: BadgeExamples())
-            Link("Buttons", target: ButtonExamples())
-            Link("Cards", target: CardExamples())
-            Link("Carousels", target: CarouselExamples())
-            Link("Code", target: CodeExamples())
-            Link("Dropdowns", target: DropdownExamples())
-            Link("Embeds", target: EmbedExamples())
-            Link("Images", target: ImageExamples())
-            Link("Includes", target: IncludeExamples())
-            Link("Links", target: LinkExamples())
-            Link("Lists", target: ListExamples())
-            Link("Quotes", target: QuoteExamples())
-            Link("Tables", target: TableExamples())
-        }
-        .listStyle(.unordered(.default))
+//        List {
+//            Link("Grid Layout", target: GridExamples())
+//            Link("Navigation", target: NavigationExamples())
+//            Link("Content", target: ContentExamples())
+//            Link("Text", target: TextExamples())
+//            Link("Styling", target: StylingExamples())
+//        }
+//        .listStyle(.ordered(.default))
+//
+//
+//        Text("Examples")
+//            .font(.title2)
+//            .margin(.top, .large)
+//
+//        List {
+//            Link("Accordions", target: AccordionExamples())
+//            Link("Alerts", target: AlertExamples())
+//            Link("Badges", target: BadgeExamples())
+//            Link("Buttons", target: ButtonExamples())
+//            Link("Cards", target: CardExamples())
+//            Link("Carousels", target: CarouselExamples())
+//            Link("Code", target: CodeExamples())
+//            Link("Dropdowns", target: DropdownExamples())
+//            Link("Embeds", target: EmbedExamples())
+//            Link("Images", target: ImageExamples())
+//            Link("Includes", target: IncludeExamples())
+//            Link("Links", target: LinkExamples())
+//            Link("Lists", target: ListExamples())
+//            Link("Quotes", target: QuoteExamples())
+//            Link("Tables", target: TableExamples())
+//        }
+//        .listStyle(.unordered(.default))
 
         Dropdown("Click Me") {
             Text("Header")
--- Sources/Site.swift
+++ Sources/Site.swift
@@ -22,35 +22,35 @@ struct ExampleSite: Site {
     var builtInIconsEnabled = true
     var syntaxHighlighters = [SyntaxHighlighter.swift, .python, .ruby]
     var feedConfiguration = FeedConfiguration(mode: .full, contentCount: 20, image: .init(url: "https://www.yoursite.com/images/icon32.png", width: 32, height: 32))
-    var robotsConfiguration = Robots()
+//    var robotsConfiguration = Robots()
     var author = "Paul Hudson"
 
     var homePage = Home()
     var tagPage = Tags()
     var theme = MyTheme()
 
-    var pages: [any StaticPage] {
-        ContentExamples()
-        GridExamples()
-        NavigationExamples()
-        TextExamples()
-
-        AccordionExamples()
-        AlertExamples()
-        BadgeExamples()
-        CardExamples()
-        CarouselExamples()
-        CodeExamples()
-        DropdownExamples()
-        EmbedExamples()
-        ImageExamples()
-        IncludeExamples()
-        LinkExamples()
-        ListExamples()
-        QuoteExamples()
-        StylingExamples()
-        TableExamples()
-    }
+//    var pages: [any StaticPage] {
+//        ContentExamples()
+//        GridExamples()
+//        NavigationExamples()
+//        TextExamples()
+//
+//        AccordionExamples()
+//        AlertExamples()
+//        BadgeExamples()
+//        CardExamples()
+//        CarouselExamples()
+//        CodeExamples()
+//        DropdownExamples()
+//        EmbedExamples()
+//        ImageExamples()
+//        IncludeExamples()
+//        LinkExamples()
+//        ListExamples()
+//        QuoteExamples()
+//        StylingExamples()
+//        TableExamples()
+//    }
 
     var layouts: [any ContentPage] {
         Story()
